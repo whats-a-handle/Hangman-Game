bab7aed8343ad7b78a378af9d4fe7b6617130582
whats-a-handle <bromanbroman54@yahoo.com>
2017-12-05 21:00:17 -0800

    Basic Play Quit Button that show hides the game content area Added footer as well

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,46 @@ $( document ).ready(function() {
 console.log("hello!");
 
 
+var masked_array = [];
+var answer_pool = ["Binturong","Platypus","Wombat"];
+var playGame = false;
+var playQuitButton = $(".play-button");
+var questions = [];
+
+var Question = {imageURL:"", hintText:"", answer:""};
+
+
+
+
+function play(){
+	if(playGame){
+		console.log("play game!");		
+		$(playQuitButton).text("Quit Playing!");
+		$(".main-row").css("visibility","visible");	
+	}
+	else
+	{
+		$(playQuitButton).text("Play Game!");
+		$(".main-row").css("visibility","hidden");
+		console.log("quitting!");
+	}
+}
+
+$(playQuitButton).on("click",function(){
+	if(playGame){
+		playGame = false;
+		play();
+	}
+	else{	
+		playGame = true;
+		play();
+	}
+});
+
+
+
+
+
 
 
 
